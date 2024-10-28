3009d06e49b04a3e2b79b26fce45d265593af5c5
Angel Guillermo <angel019124213@gmail.com>
2024-10-28 08:58:29 -0700

    Modifique las cards para que solo muestren tres parafos y sean del mismo tamano, tambien se agrego el backend con verificaciones del login y registro de usuarios

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
-const bcrypt = require('bcrypt'); // Importar bcrypt
+const bcrypt = require('bcrypt'); 
 require('dotenv').config();
 
 const app = express();
